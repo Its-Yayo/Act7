709d7f5649ac0499307e280ce6b382869ffa0248
a01754574 <a01754574@itesm.mx>
2021-11-24 10:23:48 -0600

    Commit Save

---
+++ fraccion.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+using namespace std;
+
+class Fraccion {
+    
+    private: 
+        int num;
+        int den;
+
+    public:
+        Fraccion();
+        Fraccion(int num, int den);
+
+        int getNum(); int getDen();
+
+        
+}
